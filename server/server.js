fdbf31f8ec1ed1d3d7b8b7d9d800fd20a39df65a
brahim <rajelghmn@gmail.com>
2024-09-12 22:26:56 +0000

    broadcasting to rooms instead of emitting to every channel

--- server/server.js
+++ server/server.js
@@ -11,6 +11,7 @@ import { messages, users } from './usersData.js';
 import multer from 'multer';
 import { fileURLToPath } from 'url';
 import dotenv from 'dotenv';
+import { channel } from 'node:diagnostics_channel';
 dotenv.config()
 
 const __filename = fileURLToPath(import.meta.url);
@@ -30,14 +31,18 @@ const io = new Server(server , {
 
 cloudinary.config({
     cloud_name: "dwa2csohq",
-    api_key: "665725662135347" ,
+    api_key: "665725662135347",
    api_secret:"Y2Dk2D6ExFwjQjLNlo9i6DLbq_0"
 });
 
 io.on('connection', (socket) =>{
-    console.log('user connected', 'hmm');
-    socket.on('message', (msg) => {
-        io.emit('chat', msg)
+    console.log('user connected');
+
+    socket.on('join', ( id) => {
+        socket.join( id)
+    })
+    socket.on('message', (msg,  id) => {
+        io.to( id).emit('chat', msg)
     })
  
     socket.on('disconnect', () => {
@@ -51,15 +56,16 @@ mongoose.connect(process.env.DB_URL)
 app.use(cors())
 app.use(express.json());
 
-app.post('/api/storemessage', async (req, res) => {
+app.post('/storemessage', async (req, res) => {
     try {
-
+        console.log('storing data with  id:' + req.body. id)
         const user = new messages(
              {
                  name: req.body.name,
                  message: req.body.message,
                  url: req.body.url,
-                 date: req.body.date
+                 date: req.body.date,
+                  id: req.body. id
              }
          )
          await user.save();
@@ -70,16 +76,24 @@ app.post('/api/storemessage', async (req, res) => {
     }
 })
 
-app.post('/api/storeuser', upload.single('file'), async (req, res, next) => {
+app.post('/storeuser', upload.single('file'), async (req, res, next) => {
    
         try{
-            const {name,password} = JSON.parse(req.body.user);
+            const {name,password, userName} = JSON.parse(req.body.user);
+            const check = await users.findOne({userName}).exec();
+            if(check) {
+                res.json(403).send({errorMessage: 'user already exist'});
+                console.log(check);
+                console.log('user already exist')
+                return
+            }
             const result = await cloudinary.uploader.upload(req.file.path);
             const image = result.secure_url || result.url;
             // console.log(image)
             const user = new users({
-                name: name,
-                password: password,
+                name,
+                password,
+                userName,
                 url: image
             })
             await user.save();
@@ -91,85 +105,22 @@ app.post('/api/storeuser', upload.single('file'), async (req, res, next) => {
 
 })
 
-app.get('/api/getdata', async (req, res) => {
+app.post('/getdata', async (req, res) => {
     try{
-        const data = await messages.find({}).exec();
-        res.send(data)
+        const data = await messages.find({ id: req.body. id}).exec();
+        res.status(200).json(data)
     }
     catch(err) {
         console.log('hmmm something went wrong with sending the data', err);
+        res.status(403).json({message: 'failed to get the channel data'})
     }
 })
-
-
-app.use(express.static(path.join(__dirname, '..', 'src')));
-app.use('/images', express.static('images'));
-app.use(express.static(path.join(__dirname, '..', 'dist')));
-
+app.use('/images', express.static('images'))
+app.use(express.static('dist'))
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'dist/index.html'))
 })
 
-
-
 server.listen(3000, () => {
     console.log('app running on ' + PORT)
 })
-// import cors from 'cors';
-// import session from 'express-session';
-// import cookieParser from 'cookie-parser';
-// import MongoStore from 'connect-mongo';
-//  const app = express()
-
-//  app.use(session({
-//     secret: "your-secret-key",
-//     resave:false,
-//     saveUninitialized: false,
-//     cookie: {
-//         HttpOnly: true,
-//         sameSite: 'strict',
-//     },
-//     store: MongoStore.create({
-//         mongoUrl: 'mongodb://localhost:27017/session-store',
-//     })
-//  }))
-
- 
-
-//  app.get('/login', (req, res) => {
-//     req.session.user = {id: 1, username: 'rajel'};
-//     res.send('you logged in ');
-//  });
-
-//  app.get('/', (req, res) => {
-//      if(!req?.session?.user) {
-//          res.redirect('/pls');
-//         }
-//         const sessionId = req.sessionID;
-//         console.log(req.sessionStore.get(sessionId))
-//         console.log(req.session)
-//         res.send('welcome' + " " + req.session.user.username);
-    
-//  });
-
-//  app.get('/displaydata', (req, res) => {
-//     // console.log(req.session);
-//     console.log(req.sessionStore.get(req.sessionID, (err) => {
-//         console.log
-//     }));
-//     // console.log(req.session.user);
-//     // console.log(req.store);
-//  })
-
-//  app.get('/pls', (req, res) => {
-//     res.send('please log in first mate');
-//  });
-
-//  app.get('/logout' , (req ,res) => {
-//     req.session.destroy();
-//     res.send('logout');
-//  });
-
-//  app.listen(3000, () => {
-//     console.log('app running in 3000');
-// });
